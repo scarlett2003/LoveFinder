f77a778d1a517f9956fd048762b06d64d778f925
vialley <jovilee09@gmail.com>
2015-04-15 21:15:34 +0800

    設定slider的值及轉型

--- LoveFinder/ViewController.swift
+++ LoveFinder/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var name: UITextField!
     
@@ -17,7 +17,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var birthday: UIDatePicker!
     
     @IBOutlet weak var heightNumber: UISlider!
-    @IBOutlet weak var height: UIButton!
+    
+    @IBOutlet weak var height: UILabel!
     
     @IBOutlet weak var hasProperty: UISwitch!
     
@@ -26,6 +27,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 讓按下Done後，取消鍵盤所要把delegate指定給textField自已
+        name.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,10 +39,25 @@ class ViewController: UIViewController {
 
 
     @IBAction func heightChanged(sender: AnyObject) {
+        //把sender轉型給slide
+        var slider = sender as! UISlider
+        
+        // 先設個臨時的i ,把slider的值轉成Int給i
+        var i = Int(slider.value)
+        // slider的值轉成 Float
+        slider.value = Float(i)
+        
+        height.text = "\(i)公分"
     }
     
     
     @IBAction func okTapped(sender: AnyObject) {
+    }
+    
+    //UITextFieldDeleate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
     
